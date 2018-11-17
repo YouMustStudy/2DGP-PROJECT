8840c02aef9a3074a9a76227278dea4d42cc720b
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-17 18:26:27 +0900

    UI 업데이트, 드로우 함수 추가

--- UI.py
+++ UI.py
@@ -6,6 +6,7 @@ class UI:
 
     def __init__(self, x, y, num):
         self.x, self.y = x, y
+        self.num = num
         if num == 0:
             self.image = load_image(".\\icons\\GreenUI.png")
         elif num == 1:
@@ -14,3 +15,9 @@ class UI:
             UI.cur_money_font = load_font(".\\font\\GodoB.ttf", 20)
         if UI.rank_font == None:
             UI.rank_font = load_font(".\\font\\GodoB.ttf", 20)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
