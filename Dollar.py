c91f79307e0054a43ecc05ac27c9762875c145a1
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-18 03:35:55 +0900

    달러 속도변화 추가

--- Dollar.py
+++ Dollar.py
@@ -1,29 +1,48 @@
 from pico2d import *
+from random import randint
+import game_framework
+import game_world
 
 class Dollar:
     image = None
     def __init__(self, x=400, y=400):
-        self.x, self.y = x, y
+        self.dist = randint(200, 300)
+        self.length = self.dist / 0.3
+        self.x, self.y = x, y + self.dist
+        self.timer = 1.0
         if Dollar.image == None:
             Dollar.image = load_image(".\\icons\\dollar.png")
 
     def update(self):
-        pass
+        length = min(self.dist, self.length * game_framework.frame_time)
+        self.dist -= length
+        self.y -= length
+        if length == 0:
+            self.timer -= game_framework.frame_time
+            if self.timer <= 0:
+                self.x += self.length * game_framework.frame_time
+                self.y -= self.length * game_framework.frame_time
+        if self.y < 0:
+            
+
 
     def draw(self):
         self.image.draw(self.x, self.y)
 
 
 class Bundle:
-    def __init__(self):
+    def __init__(self, x = 400, y = 400):
         self.Dollar = []
         for i in range(3):
+            for j in range(5):
+                self.Dollar.append(Dollar(x - j * 24 + 48, y - j * 15 + 5*i - 15 + 30))
+        for i in range(3):
             for j in range(5-2*i):
-                self.Dollar.append(Dollar(400 - (j+i) * 24, 400 - j * 15 + (-5)*i))
+                self.Dollar.append(Dollar(x - (j+i) * 24 + 48, y - j * 15 + (-5)*i + 30))
 
     def update(self):
-        pass
-
+        for thing in self.Dollar:
+            thing.update()
     def draw(self):
         for thing in self.Dollar:
             thing.draw()
