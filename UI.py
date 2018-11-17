3a7e120d7c5372ae2ee44488a4f09f3e4b4dd5e5
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-17 18:22:48 +0900

    UI 생성자 추가

---
+++ UI.py
@@ -0,0 +1,16 @@
+from pico2d import *
+
+class UI:
+    cur_money_font = None
+    rank_font = None
+
+    def __init__(self, x, y, num):
+        self.x, self.y = x, y
+        if num == 0:
+            self.image = load_image(".\\icons\\GreenUI.png")
+        elif num == 1:
+            self.image = load_image(".\\icons\\BlueUI.png")
+        if UI.cur_money_font == None:
+            UI.cur_money_font = load_font(".\\font\\GodoB.ttf", 20)
+        if UI.rank_font == None:
+            UI.rank_font = load_font(".\\font\\GodoB.ttf", 20)
