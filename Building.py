e9985d8221c497c51d87735f2a8c6fbdcca08017
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-18 18:05:09 +0900

    건물의 회전함수 추가

--- Building.py
+++ Building.py
@@ -1,23 +1,49 @@
 from pico2d import *
+from main_state import CENTER
+import math
+import game_world
 
 class Building:
-    Bflag = None
-    Bhouse = None
-    Bcondo = None
-    Bhotel = None
-
-    def __init__(self):
-        if Building.Bflag == None:
-            Building.Bflag = load_image(".\\building\\BlueFlag.png")
-        if Building.Bhouse == None:
-            Building.Bhouse = load_image(".\\building\\BlueHouse.png")
-        if Building.Bcondo == None:
-            Building.Bcondo = load_image(".\\building\\BlueCondo.png")
-        if Building.Bhotel == None:
-            Building.Bhotel = load_image(".\\building\\BlueHotel.png")
+    def __init__(self, level, color, tile):
+        self.x, self.y = 0, 0
+        self.theta = 0
 
     def draw(self):
         pass
 
     def update(self):
         pass
+
+    def rotate(self, theta):
+        radian=math.radians(theta)
+        self.theta += theta
+        if self.theta < 0:
+            self.theta = 360+self.theta
+        self.x-=CENTER[0]
+        self.y-=CENTER[1]
+        tmp_x, tmp_y = self.x, self.y
+        self.x=tmp_x*math.cos(radian) - tmp_y*math.sin(radian)
+        self.y=tmp_x*math.sin(radian) + tmp_y*math.cos(radian)
+        self.x+=CENTER[0]
+        self.y+=CENTER[1]
+
+class Flag:
+    Green = None
+    Blue = None
+
+    def __init__(self, color):
+        if Flag.Green == None:
+            Flag.Green = load_image('.\\building\\GreenFlag.png')
+        if Flag.Blue == None:
+            Flag.Green = load_image('.\\building\\GreenFlag.png')
+        if color == 0:
+            self.image = Flag.Green
+        else:
+            self.image = Flag.Blue
+
+    def draw(self, building):
+        self.image.rotate_draw(math.radians(building.theta), building.x, building.y)
+
+    def update(self, building):
+        if building.tile.level != 0:
+            game_world.remove_object(building)
