851c4374b7419d3a047468b3390af49e1f5bb739
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-25 17:34:32 +0900

    클릭 이벤트 발생시 이벤트 추가

--- menu_state.py
+++ menu_state.py
@@ -1,7 +1,12 @@
 from pico2d import *
 import game_framework
+import main_state
 
 image = None
+playbutton = (395, 320)
+exitbutton = (395, 180)
+button_width = 140
+button_height = 50
 
 def enter():
     global image
@@ -13,20 +18,22 @@ def exit():
 
 def update(): pass
 def draw():
-    center = (395, 320)
-    width = 140
-    height = 50
     image.draw(400, 400)
-    draw_rectangle(center[0]-width, center[1]-height,center[0]+width, center[1]+height)
+
 
 def handle_events():
     events = get_events()
     for event in events:
+        event.y = 800 - event.y + 1
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
-        elif event.type == SDL_MOUSEBUTTONDOWN:pass
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.x > playbutton[0] - button_width and event.x < playbutton[0] + button_width and event.y > playbutton[1] - button_height and event.y < playbutton[1] + button_height:
+                game_framework.change_state(main_state)
+            elif event.x > exitbutton[0] - button_width and event.x < exitbutton[0] + button_width and event.y > exitbutton[1] - button_height and event.y < exitbutton[1] + button_height:
+                game_framework.quit()
         else:pass
 
 def pause(): pass
