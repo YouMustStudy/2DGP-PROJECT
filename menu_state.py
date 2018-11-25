b5067042907e7947c8625076af7a767417957dda
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2018-11-25 17:15:03 +0900

    메뉴 진입, 퇴장 정의

--- menu_state.py
+++ menu_state.py
@@ -1,7 +1,15 @@
 from pico2d import *
 
-def enter(): pass
-def exit(): pass
+image = None
+
+def enter():
+    global image
+    image = load_image('.\\popup\\menu.png')
+
+def exit():
+    global image
+    del(image)
+
 def update(): pass
 def draw(): pass
 def handle_events(): pass
